f26c97b81aecd16a081e792d8d81fe362ba9e9b2
ceulazur <goncalvesjhordanna@gmail.com>
2025-01-12 18:58:40 -0300

    fix: reactor code smell Feature Envy using extract method and move to the appropriate class technique

--- src/main/java/org/example/studysearch/RegistrySearch.java
+++ src/main/java/org/example/studysearch/RegistrySearch.java
@@ -1,12 +1,12 @@
 package org.example.studysearch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.example.studycards.CardManager;
 import org.example.studyplanner.HabitTracker;
 import org.example.studyplanner.TodoTracker;
 import org.example.studyregistry.StudyTaskManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class RegistrySearch implements Search<String>{
     private SearchLog searchLog = new SearchLog("Registry Search");
@@ -27,9 +27,7 @@ public class RegistrySearch implements Search<String>{
         results.addAll(HabitTracker.getHabitTracker().searchInHabits(text));
         results.addAll(TodoTracker.getInstance().searchInTodos(text));
         results.addAll(StudyTaskManager.getStudyTaskManager().searchInRegistries(text));
-        this.searchLog.addSearchHistory(text);
-        this.searchLog.setNumUsages(this.searchLog.getNumUsages() + 1);
-        results.add("\nLogged in: " + this.searchLog.getLogName());
+        results.add(searchLog.logSearchAndGetName(text));
         return results;
     }
 }
--- src/main/java/org/example/studysearch/SearchLog.java
+++ src/main/java/org/example/studysearch/SearchLog.java
@@ -58,9 +58,15 @@ public class SearchLog {
     public void setLogName(String logName) {
         this.logName = logName;
     }
+
+    public void logSearch(String text) {
+        addSearchHistory(text);
+        setNumUsages(getNumUsages() + 1);
+    }
+
     public String logSearchAndGetName(String text) {
         addSearchHistory(text);
         setNumUsages(getNumUsages() + 1);
-        return getLogName();
+        return "\nLogged in: " + getLogName();
     }
 }
